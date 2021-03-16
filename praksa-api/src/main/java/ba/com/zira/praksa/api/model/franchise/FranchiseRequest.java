4e90fe4a983e4ae30e47cc3b38f87ed0bf761df8
melisa.smajovic <melissasm30@outlook.com>
2021-03-16 11:13:47 +0100

    final commit

---
+++ praksa-api/src/main/java/ba/com/zira/praksa/api/model/franchise/FranchiseRequest.java
@@ -0,0 +1,24 @@
+package ba.com.zira.praksa.api.model.franchise;
+
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class FranchiseRequest implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private String aliases;
+    private LocalDateTime created;
+    private String createdBy;
+    private String information;
+    private LocalDateTime modified;
+    private String modifiedBy;
+    private String name;
+    private String outlineText;
+ //  private Long gameId; 
+
+}
