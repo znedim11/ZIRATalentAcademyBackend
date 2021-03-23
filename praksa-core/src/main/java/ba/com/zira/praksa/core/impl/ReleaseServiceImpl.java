b0b0345f9b23b9d5ddb9b9e05262faddca6ab58c
almasafesta <afesta1@etf.unsa.ba>
2021-03-23 13:02:49 +0100

    004 Add Release

--- praksa-core/src/main/java/ba/com/zira/praksa/core/impl/ReleaseServiceImpl.java
+++ praksa-core/src/main/java/ba/com/zira/praksa/core/impl/ReleaseServiceImpl.java
@@ -15,12 +15,18 @@ import ba.com.zira.praksa.api.GameService;
 import ba.com.zira.praksa.api.PlatformService;
 import ba.com.zira.praksa.api.RegionService;
 import ba.com.zira.praksa.api.ReleaseService;
+import ba.com.zira.praksa.api.model.company.CompanyResponse;
 import ba.com.zira.praksa.api.model.game.GameResponse;
+import ba.com.zira.praksa.api.model.platform.PlatformResponse;
+import ba.com.zira.praksa.api.model.region.RegionResponse;
 import ba.com.zira.praksa.api.model.release.ReleaseRequest;
 import ba.com.zira.praksa.core.validation.ReleaseRequestValidation;
 import ba.com.zira.praksa.dao.ReleaseDAO;
 import ba.com.zira.praksa.dao.model.ReleaseEntity;
+import ba.com.zira.praksa.mapper.CompanyMapper;
 import ba.com.zira.praksa.mapper.GameMapper;
+import ba.com.zira.praksa.mapper.PlatformMapper;
+import ba.com.zira.praksa.mapper.RegionMapper;
 import ba.com.zira.praksa.mapper.ReleaseMapper;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -36,6 +42,9 @@ public class ReleaseServiceImpl implements ReleaseService
 	ReleaseMapper releaseMapper;
 	ReleaseRequestValidation releaseRequestValidation;
 	GameMapper gameMapper;
+	PlatformMapper platformMapper;
+	CompanyMapper companyMapper;
+	RegionMapper regionMapper;
 	GameService gameService;
 	CompanyService companyService;
 	PlatformService platformService;
@@ -60,12 +69,42 @@ public class ReleaseServiceImpl implements ReleaseService
 		entity.setCreatedBy(request.getUserId());
 
 		SearchRequest<Long> requestGame = new SearchRequest<>();
-		Long ID = request.getEntity().getGameId();
-		requestGame.setEntity(ID);
+		requestGame.setEntity(request.getEntity().getGameId());
+		PayloadResponse<GameResponse> responseGamePayload = new PayloadResponse<>();
+		responseGamePayload = gameService.findById(requestGame);
+		GameResponse gameResponse = responseGamePayload.getPayload();
+		entity.setGame(gameMapper.responseToEntity(gameResponse));
 
-		PayloadResponse<GameResponse> responseGame = new PayloadResponse<>();
-		responseGame = gameService.findById(requestGame);
-		// entity.setGame(gameMapper.responseToEntity(responseGame));
+		SearchRequest<Long> requestRegion = new SearchRequest<>();
+		requestRegion.setEntity(request.getEntity().getRegionId());
+		PayloadResponse<RegionResponse> responseRegionPayload = new PayloadResponse<>();
+		responseRegionPayload = regionService.findById(requestRegion);
+		RegionResponse regionResponse = responseRegionPayload.getPayload();
+		entity.setRegion(regionMapper.responseToEntity(regionResponse));
+
+		SearchRequest<Long> requestPlatform = new SearchRequest<>();
+		requestPlatform.setEntity(request.getEntity().getPlatformId());
+		PayloadResponse<PlatformResponse> responsePlatformPayload = new PayloadResponse<>();
+		responsePlatformPayload = platformService.findById(requestPlatform);
+		PlatformResponse platformResponse = responsePlatformPayload.getPayload();
+		entity.setPlatform(platformMapper.responseToEntity(platformResponse));
+
+		SearchRequest<Long> requestCompany = new SearchRequest<>();
+		PayloadResponse<CompanyResponse> responseCompanyPayload = new PayloadResponse<>();
+		if (request.getEntity().getType() == "DEV")
+		{
+			requestCompany.setEntity(request.getEntity().getDeveloperId());
+			responseCompanyPayload = companyService.findById(requestCompany);
+			CompanyResponse companyResponse = responseCompanyPayload.getPayload();
+			entity.setDeveloper(companyMapper.responseToEntity(companyResponse));
+		}
+		else if (request.getEntity().getType() == "PUB")
+		{
+			requestCompany.setEntity(request.getEntity().getPublisherId());
+			responseCompanyPayload = companyService.findById(requestCompany);
+			CompanyResponse companyResponse = responseCompanyPayload.getPayload();
+			entity.setPublisher(companyMapper.responseToEntity(companyResponse));
+		}
 
 		return new PayloadResponse<>(request, ResponseCode.OK, "Release Added Successfully");
 	}
