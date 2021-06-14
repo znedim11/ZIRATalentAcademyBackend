32cfe40a00ef6532f1fc0aab95edf3c532508874
Ajas Zornic <ajas.zornic@edu.fit.ba>
2021-06-14 11:17:39 +0200

    HUB - B - 005 Conflict Resolve: SwaggerConfiguration

---
+++ praksa-core/src/main/java/ba/com/zira/praksa/core/impl/ConceptServiceImpl.java
@@ -0,0 +1,130 @@
+/**
+ *
+ */
+package ba.com.zira.praksa.core.impl;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ba.com.zira.commons.exception.ApiException;
+import ba.com.zira.commons.message.request.EntityRequest;
+import ba.com.zira.commons.message.request.SearchRequest;
+import ba.com.zira.commons.message.response.PagedPayloadResponse;
+import ba.com.zira.commons.message.response.PayloadResponse;
+import ba.com.zira.commons.model.PagedData;
+import ba.com.zira.commons.model.response.ResponseCode;
+import ba.com.zira.commons.validation.RequestValidator;
+import ba.com.zira.praksa.api.ConceptService;
+import ba.com.zira.praksa.api.model.concept.ConceptCreateRequest;
+import ba.com.zira.praksa.api.model.concept.ConceptResponse;
+import ba.com.zira.praksa.api.model.concept.ConceptUpdateRequest;
+import ba.com.zira.praksa.core.validation.ConceptRequestValidation;
+import ba.com.zira.praksa.dao.ConceptDAO;
+import ba.com.zira.praksa.dao.model.ConceptEntity;
+import ba.com.zira.praksa.mapper.ConceptMapper;
+import lombok.AllArgsConstructor;
+
+/**
+ * @author zira
+ *
+ */
+
+@Service
+@AllArgsConstructor
+public class ConceptServiceImpl implements ConceptService {
+
+    private RequestValidator requestValidator;
+    private ConceptRequestValidation conceptRequestValidation;
+    private ConceptDAO conceptDAO;
+    private ConceptMapper conceptMapper;
+
+    @Override
+    public PagedPayloadResponse<ConceptResponse> find(SearchRequest<String> request) throws ApiException {
+        requestValidator.validate(request);
+
+        PagedData<ConceptEntity> conceptEntitesData = conceptDAO.findAll(request.getFilter());
+        List<ConceptEntity> conceptEntities = conceptEntitesData.getRecords();
+
+        final List<ConceptResponse> conceptList = conceptMapper.entityListToResponseList(conceptEntities);
+
+        PagedData<ConceptResponse> pagedData = new PagedData<ConceptResponse>();
+        pagedData.setNumberOfPages(conceptEntitesData.getNumberOfPages());
+        pagedData.setNumberOfRecords(conceptEntitesData.getNumberOfRecords());
+        pagedData.setPage(conceptEntitesData.getPage());
+        pagedData.setRecords(conceptList);
+        pagedData.setRecordsPerPage(conceptEntitesData.getRecordsPerPage());
+
+        return new PagedPayloadResponse<ConceptResponse>(request, ResponseCode.OK, pagedData);
+    }
+
+    @Override
+    public PayloadResponse<ConceptResponse> findById(SearchRequest<Long> request) throws ApiException {
+        EntityRequest<Long> entityRequest = new EntityRequest<Long>(request.getEntity(), request);
+        conceptRequestValidation.validateConceptExists(entityRequest, "validateAbstractRequest");
+
+        final ConceptEntity conceptEntity = conceptDAO.findByPK(request.getEntity());
+
+        final ConceptResponse conceptResponse = conceptMapper.entityToResponse(conceptEntity);
+
+        return new PayloadResponse<ConceptResponse>(request, ResponseCode.OK, conceptResponse);
+    }
+
+    @Override
+    @Transactional(rollbackFor = ApiException.class)
+    public PayloadResponse<ConceptResponse> create(EntityRequest<ConceptCreateRequest> request) throws ApiException {
+        conceptRequestValidation.validateEntityExistsInCreateRequest(request, "basicNotNull");
+
+        EntityRequest<String> entityRequest = new EntityRequest<String>(request.getEntity().getName(), request);
+        conceptRequestValidation.validateConceptNameExists(entityRequest, "basicNotNull");
+
+        ConceptEntity entity = conceptMapper.createRequestToEntity(request.getEntity());
+        entity.setCreated(LocalDateTime.now());
+        entity.setCreatedBy(request.getUserId());
+
+        conceptDAO.persist(entity);
+
+        ConceptResponse response = conceptMapper.entityToResponse(entity);
+
+        return new PayloadResponse<ConceptResponse>(request, ResponseCode.OK, response);
+    }
+
+    @Override
+    @Transactional(rollbackFor = ApiException.class)
+    public PayloadResponse<ConceptResponse> update(final EntityRequest<ConceptUpdateRequest> request) throws ApiException {
+        conceptRequestValidation.validateEntityExistsInUpdateRequest(request, "basicNotNull");
+
+        EntityRequest<Long> entityRequestId = new EntityRequest<Long>(request.getEntity().getId(), request);
+        conceptRequestValidation.validateConceptExists(entityRequestId, "validateAbstractRequest");
+
+        EntityRequest<String> entityRequestName = new EntityRequest<String>(request.getEntity().getName(), request);
+        conceptRequestValidation.validateConceptNameExists(entityRequestName, "basicNotNull");
+
+        final ConceptUpdateRequest conceptRequest = request.getEntity();
+
+        ConceptEntity conceptEntity = conceptDAO.findByPK(request.getEntity().getId());
+        conceptEntity = conceptMapper.updateRequestToEntity(conceptRequest, conceptEntity);
+        conceptEntity.setModified(LocalDateTime.now());
+        conceptEntity.setModifiedBy(request.getUserId());
+
+        conceptDAO.merge(conceptEntity);
+
+        final ConceptResponse conceptResponse = conceptMapper.entityToResponse(conceptEntity);
+
+        return new PayloadResponse<ConceptResponse>(request, ResponseCode.OK, conceptResponse);
+    }
+
+    @Override
+    @Transactional(rollbackFor = ApiException.class)
+    public PayloadResponse<String> delete(EntityRequest<Long> request) throws ApiException {
+        EntityRequest<Long> entityRequest = new EntityRequest<Long>(request.getEntity(), request);
+        conceptRequestValidation.validateConceptExists(entityRequest, "validateAbstractRequest");
+
+        conceptDAO.removeByPK(request.getEntity());
+
+        return new PayloadResponse<String>(request, ResponseCode.OK, "Concept deleted!");
+    }
+
+}
---
+++ praksa-core/src/main/java/ba/com/zira/praksa/core/validation/ConceptRequestValidation.java
@@ -0,0 +1,78 @@
+/**
+ *
+ */
+package ba.com.zira.praksa.core.validation;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Component;
+
+import ba.com.zira.commons.message.request.EntityRequest;
+import ba.com.zira.commons.message.response.ValidationResponse;
+import ba.com.zira.commons.model.response.ResponseCode;
+import ba.com.zira.commons.validation.RequestValidator;
+import ba.com.zira.praksa.api.model.concept.ConceptCreateRequest;
+import ba.com.zira.praksa.api.model.concept.ConceptUpdateRequest;
+import ba.com.zira.praksa.dao.ConceptDAO;
+import lombok.AllArgsConstructor;
+
+/**
+ * @author zira
+ *
+ */
+
+@Component("componentRequestValidation")
+@AllArgsConstructor
+public class ConceptRequestValidation {
+    private RequestValidator requestValidator;
+    private ConceptDAO conceptDAO;
+
+    public ValidationResponse validateConceptExists(final EntityRequest<Long> request, final String validationRuleMessage) {
+        ValidationResponse validationResponse = requestValidator.validate(request, validationRuleMessage);
+        if (validationResponse.getResponseCode() == ResponseCode.OK.getCode()) {
+            StringBuilder errorDescription = new StringBuilder();
+            if (!conceptDAO.existsByPK(request.getEntity())) {
+                errorDescription.append("Concept with id ").append(request.getEntity()).append(" does not exist !");
+            }
+            validationResponse = requestValidator.createResponse(request, errorDescription);
+        }
+        return validationResponse;
+    }
+
+    public ValidationResponse validateConceptNameExists(final EntityRequest<String> request, final String validationRuleMessage) {
+        ValidationResponse validationResponse = requestValidator.validate(request, validationRuleMessage);
+        if (validationResponse.getResponseCode() == ResponseCode.OK.getCode()) {
+            StringBuilder errorDescription = new StringBuilder();
+            if (StringUtils.isBlank(request.getEntity())) {
+                errorDescription.append("Concept must have name!");
+            }
+            validationResponse = requestValidator.createResponse(request, errorDescription);
+        }
+        return validationResponse;
+    }
+
+    public ValidationResponse validateEntityExistsInCreateRequest(final EntityRequest<ConceptCreateRequest> request,
+            final String validationRuleMessage) {
+        ValidationResponse validationResponse = requestValidator.validate(request, validationRuleMessage);
+        if (validationResponse.getResponseCode() == ResponseCode.OK.getCode()) {
+            StringBuilder errorDescription = new StringBuilder();
+            if (request.getEntity() == null) {
+                errorDescription.append("Entity must exist in request!");
+            }
+            validationResponse = requestValidator.createResponse(request, errorDescription);
+        }
+        return validationResponse;
+    }
+
+    public ValidationResponse validateEntityExistsInUpdateRequest(final EntityRequest<ConceptUpdateRequest> request,
+            final String validationRuleMessage) {
+        ValidationResponse validationResponse = requestValidator.validate(request, validationRuleMessage);
+        if (validationResponse.getResponseCode() == ResponseCode.OK.getCode()) {
+            StringBuilder errorDescription = new StringBuilder();
+            if (request.getEntity() == null) {
+                errorDescription.append("Entity must exist in request!");
+            }
+            validationResponse = requestValidator.createResponse(request, errorDescription);
+        }
+        return validationResponse;
+    }
+}
