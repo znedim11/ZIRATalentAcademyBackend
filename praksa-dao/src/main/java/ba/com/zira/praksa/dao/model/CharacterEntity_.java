053b36f6303b94275ea6e6df2b329f589a4e4f9f
Irma Masleša <mesadi21@gmail.com>
2021-06-15 10:02:38 +0200

    Fixes

--- praksa-dao/src/main/java/ba/com/zira/praksa/dao/model/CharacterEntity_.java
+++ praksa-dao/src/main/java/ba/com/zira/praksa/dao/model/CharacterEntity_.java
@@ -3,6 +3,7 @@ package ba.com.zira.praksa.dao.model;
 import java.time.LocalDateTime;
 
 import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
@@ -10,20 +11,17 @@ import javax.persistence.metamodel.StaticMetamodel;
 @StaticMetamodel(CharacterEntity.class)
 public abstract class CharacterEntity_ {
     public static volatile SingularAttribute<CharacterEntity, Long> id;
+    public static volatile SingularAttribute<CharacterEntity, String> aliases;
     public static volatile SingularAttribute<CharacterEntity, LocalDateTime> created;
     public static volatile SingularAttribute<CharacterEntity, String> createdBy;
     public static volatile SingularAttribute<CharacterEntity, LocalDateTime> modified;
-    // public static volatile SingularAttribute<CharacterEntity,
-    // CharacterEntity> character;
-    // public static volatile SingularAttribute<CharacterEntity, ConceptEntity>
-    // concept;
-    // public static volatile SingularAttribute<CharacterEntity, GameEntity>
-    // game;
-    // public static volatile SingularAttribute<CharacterEntity, LocationEntity>
-    // location;
-    // public static volatile SingularAttribute<CharacterEntity, ObjectEntity>
-    // object;
-    // public static volatile SingularAttribute<CharacterEntity, PersonEntity>
-    // person;
+    public static volatile SingularAttribute<CharacterEntity, String> modifiedBy;
+    public static volatile SingularAttribute<CharacterEntity, LocalDateTime> dob;
+    public static volatile SingularAttribute<CharacterEntity, LocalDateTime> dod;
+    public static volatile SingularAttribute<CharacterEntity, String> gender;
+    public static volatile SingularAttribute<CharacterEntity, String> information;
+    public static volatile SingularAttribute<CharacterEntity, String> name;
+    public static volatile SingularAttribute<CharacterEntity, String> realName;
+    public static volatile ListAttribute<CharacterEntity, LinkMapEntity> linkMaps;
 
 }
