dbb7102ef0cc7a26f9b2eba7dfb459422d15a9b4
Dado <Dado.Bajramovic@zira.com.ba>
2021-06-15 13:16:14 +0200

    Added ComponentScan

--- praksa-service/src/main/java/ba/com/zira/praksa/configuration/ApplicationConfiguration.java
+++ praksa-service/src/main/java/ba/com/zira/praksa/configuration/ApplicationConfiguration.java
@@ -2,11 +2,14 @@ package ba.com.zira.praksa.configuration;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
 import ba.com.zira.commons.configuration.BaseApplicationConfiguration;
 
 @EnableEurekaClient
 @SpringBootApplication
+@ComponentScan(basePackages = { "ba.com.zira.praksa.model", "ba.com.zira.praksa.configuration", "ba.com.zira.praksa.dao",
+        "ba.com.zira.praksa.core.impl", "ba.com.zira.praksa.rest", "ba.com.zira.praksa.core.validation", "ba.com.zira.praksa.mapper" })
 public class ApplicationConfiguration extends BaseApplicationConfiguration {
 
 }
