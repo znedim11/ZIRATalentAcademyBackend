eb783401bb55be4ac5a592104a7cc47b644a43cd
Nermina Sahbegovic <nermina.sahbegovic1@gmail.com>
2021-06-24 09:13:20 +0200

    HUB-B-019 GET Games, Objects, Concepts, Characters for Person

--- praksa-core/src/main/java/ba/com/zira/praksa/core/impl/PersonServiceImpl.java
+++ praksa-core/src/main/java/ba/com/zira/praksa/core/impl/PersonServiceImpl.java
@@ -9,18 +9,31 @@ import org.springframework.transaction.annotation.Transactional;
 import ba.com.zira.commons.exception.ApiException;
 import ba.com.zira.commons.message.request.EntityRequest;
 import ba.com.zira.commons.message.request.SearchRequest;
+import ba.com.zira.commons.message.response.ListPayloadResponse;
 import ba.com.zira.commons.message.response.PagedPayloadResponse;
 import ba.com.zira.commons.message.response.PayloadResponse;
 import ba.com.zira.commons.model.PagedData;
 import ba.com.zira.commons.model.response.ResponseCode;
 import ba.com.zira.commons.validation.RequestValidator;
 import ba.com.zira.praksa.api.PersonService;
+import ba.com.zira.praksa.api.model.character.CharacterResponse;
+import ba.com.zira.praksa.api.model.concept.ConceptResponse;
+import ba.com.zira.praksa.api.model.game.GameResponse;
+import ba.com.zira.praksa.api.model.object.ObjectResponse;
 import ba.com.zira.praksa.api.model.person.Person;
 import ba.com.zira.praksa.api.model.person.PersonCreateRequest;
 import ba.com.zira.praksa.api.model.person.PersonUpdateRequest;
 import ba.com.zira.praksa.core.validation.PersonRequestValidation;
 import ba.com.zira.praksa.dao.PersonDAO;
+import ba.com.zira.praksa.dao.model.CharacterEntity;
+import ba.com.zira.praksa.dao.model.ConceptEntity;
+import ba.com.zira.praksa.dao.model.GameEntity;
+import ba.com.zira.praksa.dao.model.ObjectEntity;
 import ba.com.zira.praksa.dao.model.PersonEntity;
+import ba.com.zira.praksa.mapper.CharacterMapper;
+import ba.com.zira.praksa.mapper.ConceptMapper;
+import ba.com.zira.praksa.mapper.GameMapper;
+import ba.com.zira.praksa.mapper.ObjectMapper;
 import ba.com.zira.praksa.mapper.PersonMapper;
 
 @Service
@@ -30,15 +43,25 @@ public class PersonServiceImpl implements PersonService {
     PersonRequestValidation personRequestValidation;
     PersonDAO personDAO;
     PersonMapper personMapper;
+    GameMapper gameMapper;
+    ConceptMapper conceptMapper;
+    CharacterMapper characterMapper;
+    ObjectMapper objectMapper;
+
     static final String VALIDATION_RULE = "validateAbstractRequest";
 
     public PersonServiceImpl(RequestValidator requestValidator, PersonRequestValidation personRequestValidation, PersonDAO personDAO,
-            PersonMapper personMapper) {
+            PersonMapper personMapper, GameMapper gameMapper, ConceptMapper conceptMapper, CharacterMapper characterMapper,
+            ObjectMapper objectMapper) {
         super();
         this.requestValidator = requestValidator;
         this.personRequestValidation = personRequestValidation;
         this.personDAO = personDAO;
         this.personMapper = personMapper;
+        this.gameMapper = gameMapper;
+        this.conceptMapper = conceptMapper;
+        this.characterMapper = characterMapper;
+        this.objectMapper = objectMapper;
     }
 
     @Override
@@ -108,6 +131,33 @@ public class PersonServiceImpl implements PersonService {
 
         personDAO.removeByPK(request.getEntity());
         return new PayloadResponse<>(request, ResponseCode.OK, "Person deleted!");
+    }
+
+    @Override
+    public ListPayloadResponse<GameResponse> getGamesForPerson(EntityRequest<Long> request) throws ApiException {
+        List<GameEntity> listOfGameEntities = personDAO.getGamesForPerson(request.getEntity());
+
+        return new ListPayloadResponse<>(request, ResponseCode.OK, gameMapper.gameEntitesToGames(listOfGameEntities));
+    }
+
+    @Override
+    public ListPayloadResponse<ConceptResponse> getConceptsForPerson(EntityRequest<Long> request) throws ApiException {
+        List<ConceptEntity> listOfConceptEntities = personDAO.getConceptsForPerson(request.getEntity());
+
+        return new ListPayloadResponse<>(request, ResponseCode.OK, conceptMapper.entityListToResponseList(listOfConceptEntities));
+    }
+
+    @Override
+    public ListPayloadResponse<ObjectResponse> getObjectsForPerson(EntityRequest<Long> request) throws ApiException {
+        List<ObjectEntity> listOfObjectEntities = personDAO.getObjectsForPerson(request.getEntity());
+        return new ListPayloadResponse<>(request, ResponseCode.OK, objectMapper.entityListToDtoList(listOfObjectEntities));
+    }
+
+    @Override
+    public ListPayloadResponse<CharacterResponse> getCharactersForPerson(EntityRequest<Long> request) throws ApiException {
+        List<CharacterEntity> listOfCharacterEntities = personDAO.getCharactersForPerson(request.getEntity());
+        return new ListPayloadResponse<>(request, ResponseCode.OK, characterMapper.entityListToDtoList(listOfCharacterEntities));
+
     }
 
 }
--- praksa-dao/src/main/java/ba/com/zira/praksa/dao/PersonDAO.java
+++ praksa-dao/src/main/java/ba/com/zira/praksa/dao/PersonDAO.java
@@ -1,11 +1,63 @@
 package ba.com.zira.praksa.dao;
 
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
 import org.springframework.stereotype.Repository;
 
 import ba.com.zira.commons.dao.AbstractDAO;
+import ba.com.zira.praksa.dao.model.CharacterEntity;
+import ba.com.zira.praksa.dao.model.ConceptEntity;
+import ba.com.zira.praksa.dao.model.GameEntity;
+import ba.com.zira.praksa.dao.model.ObjectEntity;
 import ba.com.zira.praksa.dao.model.PersonEntity;
 
 @Repository
 public class PersonDAO extends AbstractDAO<PersonEntity, Long> {
+
+    static final String PERSON_ID = "personId";
+
+    public List<GameEntity> getGamesForPerson(final Long personId) {
+
+        TypedQuery<GameEntity> query = entityManager.createQuery(
+                "SELECT g FROM GameEntity g, LinkMapEntity lm WHERE lm.person.id= :personId AND lm.game.id= g.id", GameEntity.class);
+
+        query.setParameter(PERSON_ID, personId);
+        return query.getResultList();
+
+    }
+
+    public List<ConceptEntity> getConceptsForPerson(final Long personId) {
+
+        TypedQuery<ConceptEntity> query = entityManager.createQuery(
+                "SELECT c FROM ConceptEntity c, LinkMapEntity lm WHERE lm.person.id= :personId AND lm.concept.id= c.id",
+                ConceptEntity.class);
+
+        query.setParameter(PERSON_ID, personId);
+        return query.getResultList();
+
+    }
+
+    public List<ObjectEntity> getObjectsForPerson(final Long personId) {
+
+        TypedQuery<ObjectEntity> query = entityManager.createQuery(
+                "SELECT o FROM ObjectEntity o, LinkMapEntity lm WHERE lm.person.id= :personId AND lm.object.id= c.id", ObjectEntity.class);
+
+        query.setParameter(PERSON_ID, personId);
+        return query.getResultList();
+
+    }
+
+    public List<CharacterEntity> getCharactersForPerson(final Long personId) {
+
+        TypedQuery<CharacterEntity> query = entityManager.createQuery(
+                "SELECT c FROM CharacterEntity c, LinkMapEntity lm WHERE lm.person.id= :personId AND lm.character.id= c.id",
+                CharacterEntity.class);
+
+        query.setParameter(PERSON_ID, personId);
+        return query.getResultList();
+
+    }
 
 }
