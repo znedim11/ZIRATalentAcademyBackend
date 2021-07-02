857da735c939b46e5ecd4fda6fc8ee0e146d6f0d
Vedad Ganic <ganicved@gmail.com>
2021-07-02 09:20:14 +0200

    Validation done

--- praksa-core/src/main/java/ba/com/zira/praksa/mapper/MediaMapper.java
+++ praksa-core/src/main/java/ba/com/zira/praksa/mapper/MediaMapper.java
@@ -1,11 +1,13 @@
 package ba.com.zira.praksa.mapper;
 
 import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 import ba.com.zira.praksa.api.model.media.Media;
 import ba.com.zira.praksa.dao.model.MediaEntity;
 
+@Mapper(componentModel = "spring")
 public interface MediaMapper {
 
     @InheritInverseConfiguration(name = "dtoToEntity")
@@ -18,12 +20,12 @@ public interface MediaMapper {
     /*
      * @Mapping(source = "fullName", target = "fullName") Game
      * gameEntityToGame(GameEntity sampleModelEntity);
-     * 
+     *
      * @Mapping(source = "fullName", target = "fullName") GameEntity
      * gameToGameEntity(Game sampleModel);
-     * 
+     *
      * GameEntity dtoToEntity(Game sample);
-     * 
+     *
      * @InheritInverseConfiguration(name = "dtoToEntity") Game
      * entityToDto(GameEntity billingPeriodEntity);
      */
