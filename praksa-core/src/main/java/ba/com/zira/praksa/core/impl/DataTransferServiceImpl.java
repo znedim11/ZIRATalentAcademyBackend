6739d2b0451ab714b45c0043b9a49e3a9cd6de85
Irma Masleša <mesadi21@gmail.com>
2021-07-08 23:24:16 +0200

    Platform abbrevation creation

--- praksa-core/src/main/java/ba/com/zira/praksa/core/impl/DataTransferServiceImpl.java
+++ praksa-core/src/main/java/ba/com/zira/praksa/core/impl/DataTransferServiceImpl.java
@@ -1,7 +1,11 @@
 package ba.com.zira.praksa.core.impl;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.apache.commons.lang.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -11,8 +15,12 @@ import ba.com.zira.commons.exception.ApiException;
 import ba.com.zira.commons.message.request.EmptyRequest;
 import ba.com.zira.commons.message.response.PayloadResponse;
 import ba.com.zira.praksa.api.DataTransferService;
+import ba.com.zira.praksa.dao.CompanyDAO;
+import ba.com.zira.praksa.dao.PlatformDAO;
 import ba.com.zira.praksa.dao.TransferCompanyDAO;
 import ba.com.zira.praksa.dao.TransferPlatformDAO;
+import ba.com.zira.praksa.dao.model.CompanyEntity;
+import ba.com.zira.praksa.dao.model.PlatformEntity;
 import ba.com.zira.praksa.dao.model.TransferCompanyEntity;
 import ba.com.zira.praksa.dao.model.TransferPlatformEntity;
 
@@ -22,11 +30,16 @@ public class DataTransferServiceImpl implements DataTransferService {
     private static final Logger LOGGER = LoggerFactory.getLogger(DataTransferServiceImpl.class);
     TransferPlatformDAO transferPlatformDAO;
     TransferCompanyDAO transferCompanyDAO;
+    PlatformDAO platformDAO;
+    CompanyDAO companyDAO;
 
-    public DataTransferServiceImpl(TransferPlatformDAO transferPlatformDAO, TransferCompanyDAO transferCompanyDAO) {
+    public DataTransferServiceImpl(TransferPlatformDAO transferPlatformDAO, TransferCompanyDAO transferCompanyDAO, PlatformDAO platformDAO,
+            CompanyDAO companyDAO) {
         super();
         this.transferPlatformDAO = transferPlatformDAO;
         this.transferCompanyDAO = transferCompanyDAO;
+        this.platformDAO = platformDAO;
+        this.companyDAO = companyDAO;
     }
 
     @Override
@@ -35,10 +48,77 @@ public class DataTransferServiceImpl implements DataTransferService {
         List<TransferPlatformEntity> transferPlatformEntities = transferPlatformDAO.findAll();
         List<TransferCompanyEntity> transferCompanyEntities = transferCompanyDAO.findAll();
 
-        LOGGER.info("HUS_PLATFORM has {} records. \nHUS_COMPANY_MAPPING has {} records.", transferPlatformEntities.size(),
-                transferCompanyEntities.size());
+        Map<String, List<PlatformEntity>> platformEntities = platformDAO.findAll().stream()
+                .collect(Collectors.groupingBy(PlatformEntity::getFullName));
+        Map<String, List<CompanyEntity>> companyEntites = companyDAO.findAll().stream()
+                .collect(Collectors.groupingBy(CompanyEntity::getName));
 
+        for (TransferPlatformEntity transferPlatform : transferPlatformEntities) {
+            PlatformEntity platformEntity = new PlatformEntity();
+            platformEntity.setFullName(transferPlatform.getName());
+            platformEntity.setAbbriviation(makeAbbrevation(transferPlatform.getName()));
+            checkCompanyName(transferPlatform.getDeveloper(), transferCompanyEntities, companyEntites);
+
+        }
         return null;
+    }
+
+    private CompanyEntity checkCompanyName(String developer, List<TransferCompanyEntity> transferCompanyEntities,
+            Map<String, List<CompanyEntity>> companyEntites) {
+        String companyName = null;
+        if (developer != null) {
+            for (TransferCompanyEntity transferCompany : transferCompanyEntities) {
+                String[] names = transferCompany.getAllNames().split("#");
+                for (String name : names) {
+                    if (name.equalsIgnoreCase(developer)) {
+                        companyName = transferCompany.getRootName();
+                        break;
+                    }
+                }
+            }
+
+            if (companyEntites.get(companyName) == null) {
+                return createCompany(companyName);
+
+            } else {
+                LOGGER.info("Existing Company --> {}", companyEntites.get(companyName).get(0));
+                return companyEntites.get(companyName).get(0);
+            }
+        } else {
+            return null;
+        }
+
+    }
+
+    private CompanyEntity createCompany(String companyName) {
+        CompanyEntity company = new CompanyEntity();
+        company.setName(companyName);
+        company.setCreated(LocalDateTime.now());
+        company.setCreatedBy("DTS");
+        companyDAO.persist(company);
+        LOGGER.info("Created Company --> {}", company.getName());
+
+        return company;
+    }
+
+    private String makeAbbrevation(String name) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (name != null && !name.equals("") && !name.trim().isEmpty()) {
+            String[] words = name.split(" ");
+
+            if (words.length > 1) {
+                for (String word : words) {
+                    if (NumberUtils.isNumber(word)) {
+                        stringBuilder.append(word);
+                    } else {
+                        stringBuilder.append(word.toUpperCase().charAt(0));
+                    }
+                }
+            } else {
+                stringBuilder.append(words[0].toUpperCase().substring(0, 3));
+            }
+        }
+        return stringBuilder.toString();
     }
 
     @Override
