778a3331b4eac2012be8f073217f619f581227ec
Irma Masleša <mesadi21@gmail.com>
2021-07-16 13:00:56 +0200

    Revert "Implemented get LoV for Location, Object and Person"
    This reverts commit 760fb9e47cc5af5c510ef7d49988cab8850bf633.

--- praksa-dao/src/main/java/ba/com/zira/praksa/dao/LocationDAO.java
+++ praksa-dao/src/main/java/ba/com/zira/praksa/dao/LocationDAO.java
@@ -1,28 +1,11 @@
 package ba.com.zira.praksa.dao;
 
-import java.util.List;
-
-import javax.persistence.TypedQuery;
-
 import org.springframework.stereotype.Repository;
 
 import ba.com.zira.commons.dao.AbstractDAO;
-import ba.com.zira.praksa.api.model.LoV;
 import ba.com.zira.praksa.dao.model.LocationEntity;
 
 @Repository
 public class LocationDAO extends AbstractDAO<LocationEntity, Long> {
-    public List<LoV> getLoVs(List<Long> list) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(String.format("SELECT new ba.com.zira.praksa.api.model.LoV(l.id, l.name) FROM LocationEntity l %s",
-                list != null ? "WHERE l.id IN :list" : ""));
-
-        TypedQuery<LoV> query = entityManager.createQuery(stringBuilder.toString(), LoV.class);
-        if (list != null) {
-            query.setParameter("list", list);
-        }
-
-        return query.getResultList();
-    }
 
 }
